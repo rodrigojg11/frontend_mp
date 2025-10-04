7070654a53626b463d41730495f9bbb25dd7c93f
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-10-03 21:36:22 -0500

    navbar & footer

---
+++ js/token.js
@@ -0,0 +1,43 @@
+const login = document.getElementById('login')
+const logout = document.getElementById('logout')
+
+  if (!sessionStorage.getItem("token")) {
+    login.classList.add("d-none")
+    }
+  else {
+    logout.classList.add("d-none")
+  }
+
+
+form.addEventListener('submit', (e) => {
+
+  e.preventDefault()
+
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  if (!sessionStorage.getItem("token")) {
+
+    fetch("http://127.0.0.1:3000/api/v1/tokens",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({'user': {
+        email: email,
+        password: password
+      }})
+    })
+    .then(response => response.json())
+    .then((data) => {
+      sessionStorage.setItem("token", data.token)
+    })
+  }
+  else{
+    const message = document.getElementById('message')
+      message.innerText = "Usuario logueado"
+      message.style.color = "red"
+    }
+
+})
